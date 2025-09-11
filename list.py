ee6062e7837d7a4b0f1f48f5b827a1397a9de4b9
abdiaelema76 <gababoabdiaelema@gmail.com>
2025-09-11 08:56:40 +0300

    lists

---
+++ list.py
@@ -0,0 +1,9 @@
+l = [1,2,3,4,5]
+print(l) #prints the whole list
+print(l[4]) #prints value at specified index
+
+names = ["Abdia","Chaltu","Insene","Lokho"]
+print(names)
+print(names[0:3]) # using slices
+print ([names [i] for i in [1,2]]) #Using a list comprehension (for specific indices):
+print (names[1], names[2]) #Accessing individually
