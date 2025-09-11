ae9de63c9159280e1cbd9b00a8610ff4405c35bf
abdiaelema76 <gababoabdiaelema@gmail.com>
2025-09-11 09:33:47 +0300

    Add sublist creation, list concatenation, and traversal examples

---
+++ sublist.py
@@ -0,0 +1,16 @@
+produce = ["mangoes", "oranges", "bananas", "kales","tomatoes","spinach"]
+fruits = produce[0:3]
+vegies = produce[3:]
+print(produce)
+print(fruits)
+print(vegies)
+
+#concatenation
+list1 = ["a","b","c"]
+list2 = [1,2,3]
+print(list1 + list2)
+
+# traverse a list element-by-element:
+numbers =[1,6,3,5,9,10]
+for val in numbers:
+    print(val)
