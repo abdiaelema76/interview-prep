26d5ed8e641b46b641a3c039a5507d5f29bd9f1c
abdiaelema76 <gababoabdiaelema@gmail.com>
2025-09-26 08:38:00 +0300

    a function to find a max value of a varible

---
+++ max.py
@@ -0,0 +1,24 @@
+"""
+Problem Statement
+Implement the findMaximum function that receives two numbers as arguments x and y and returns the maximum of the numbers.
+
+Input
+Two numbers x and y
+
+Output
+The maximum of two numbers x and y
+
+Sample Input
+x = 2, y = 3
+
+Sample Output
+3
+"""
+def findMaximum(x, y):
+  max2 = 0
+  if(x > y):
+    max2 = x
+  else:
+    max2 = y
+  return max2
+print(findMaximum(3, 9))
